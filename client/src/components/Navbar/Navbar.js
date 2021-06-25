fd02d42a0160b507d865c11c648d3e57931dac38
Anurag Dobhal Git <dobhalannu246@gmail.com>
2021-06-25 21:25:48 +0530

    update

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -22,7 +22,7 @@ function Navbar() {
 
   <Link to="./">
           <div>
-            <lable className="logo">Anurag Dobhal</lable>
+            <lable className="logo">Anurag</lable>
           </div>
   </Link>
         
